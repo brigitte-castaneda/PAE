dfa61976abeb5b2d6ecfccd47371ad98e518ba09
JAIME ANDRES POLANCO JIMENEZ <jaime.polanco@javeriana.edu.co>
2023-03-11 18:44:57 -0500

    2023-11-03

--- Scripts/Python/cvrp_by_etc.py
+++ Scripts/Python/cvrp_by_etc.py
@@ -28,6 +28,38 @@ def create_data_model():
     return data
 
 
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    print(f'Objective: {solution.ObjectiveValue()}')
+    total_distance = 0
+    total_load = 0
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        route_load = 0
+        while not routing.IsEnd(index):
+            node_index = manager.IndexToNode(index)
+            route_load += data['demands'][node_index]
+            plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+            
+        plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(  index),
+                                                 route_load)
+        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        plan_output += 'Load of the route: {}\n'.format(route_load)
+        print(plan_output)
+        
+        total_distance += route_distance
+        total_load += route_load
+    print('Total distance of all routes: {}m'.format(total_distance))
+    print('Total load of all routes: {}'.format(total_load))
+
+
+
 def create_solution_df(data, manager, routing, solution):
     """Returns solution as a Pandas DataFrame."""
     df = pd.DataFrame(columns=["node_index", "vehicle_id", "route_load", "load" , "position", "route_distance", "route_positions"])
